420b99fce587f760bb68a97a9b64ef54e7bc9b5f
Nathaniel Graham <ngraham20@my.whitworth.edu>
2017-11-11 14:25:20 -0800

    -> added achievement class

---
+++ ocean_mine/src/ocean_mine/Achievement.java
@@ -0,0 +1,28 @@
+/*
+ * Project created by Nathaniel Graham and Sam Holzer
+ */
+package ocean_mine;
+
+/**
+ *
+ * @author ngraham20
+ */
+public class Achievement {
+    
+    public enum List
+    {
+        Drop_The_Bass,
+        Help_Me,
+        Taking_Inventory
+    }
+    
+    Achievement()
+    {
+        
+    }
+
+    public void unlockAchievement(List achievement)
+    {
+        
+    }
+}
